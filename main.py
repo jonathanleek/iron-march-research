05be3a67f19ed581fd31ab0d6862d3b5ed4c6ec6
Jonathan Leek <jonathanleek@gmail.com>
2020-01-06 10:37:31 -0600

    First keyword search

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import connection
 print(connection.mydb)
 
+mycursor = connection.mydb.cursor()
+
+mycursor.execute(
+    "SELECT * FROM forums_posts where post like '%male%' "
+)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+    print(x)
