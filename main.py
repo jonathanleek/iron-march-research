096f890b21f167fbc9888d5a29df8c4b9b26fc49
Jonathan Leek <jonathanleek@gmail.com>
2020-01-31 14:12:34 -0600

    Outputs to html files

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
 import connection
-print(connection.mydb)
+import mysql.connector
 
-mycursor = connection.mydb.cursor()
-search_words = ["male", "sex", "feminist"]
+
+search_words = ["alpha", "AoC", "beta", "boy", "consent", "cuck", "cuckold", "fag", "female", "feminazi", "feminine", "feminism", "feminist", "gay", "gender", "girl", "incel", "male", "mangina", "masculine", "rights", "misogyny", "pill", "rape", "sex", "sexuality", "victim"]
 
 # TODO Separate functions for public posts vs private messages
 # TODO Modify to iterate through a provided csv of search words
 
-def word_search(columns, table, search_column, search_term):
-    #Creates a SELECT/FROM/WHERE/LIKE query against the database.
+def word_search(columns, table, search_column, search_term, type):
+    # Creates a SELECT/FROM/WHERE/LIKE query against the database.
+    mydb = mysql.connector.connect(
+        host=connection.myhost,
+        user=connection.myuser,
+        database=connection.mydatabase,
+        password=connection.mypassword
+    )
+
+    mycursor = mydb.cursor()
     mycursor.execute(
         "SELECT " + columns + " FROM " + table + " WHERE " + search_column + " LIKE '%" + search_term + "%'"
-        #TODO improve search function https://dev.mysql.com/doc/refman/8.0/en/fulltext-search.html
+        # TODO improve search function https://dev.mysql.com/doc/refman/8.0/en/fulltext-search.html
     )
     myresult = mycursor.fetchall()
-    # TODO Output to csv
+    rownum=0
     for x in myresult:
-        print(x)
+        rownum=rownum+1
+        with open('/Users/jonathanleek/Desktop/working/'+type+'_'+search_term+'_'+str(rownum)+'.html', 'w') as out:
+            print(x, file=out)
+    mycursor.close()
 
-def public_post_search(search_terms):
-    for i in search_terms:
-        word_search("author_name, post_date, post", "forums_posts", "post", i)
+for word in search_words:
+    print("Public Post Results for " + word)
+    word_search("post", "forums_posts", "post", word,"public")
+    print("Private Message Results for " + word)
+    word_search("msg_post", "core_message_posts", "msg_post", word, "private")
 
-public_post_search(search_words)
--- test.py
+++ test.py
@@ -2,14 +2,12 @@ import connection
 import mysql.connector
 
 
-
-
-search_words = ["male","female"]
+search_words = ["alpha", "AoC", "beta", "boy", "consent", "cuck", "cuckold", "fag", "female", "feminazi", "feminine", "feminism", "feminist", "gay", "gender", "girl", "incel", "male", "mangina", "masculine", "rights", "misogyny", "pill", "rape", "sex", "sexuality", "victim"]
 
 # TODO Separate functions for public posts vs private messages
 # TODO Modify to iterate through a provided csv of search words
 
-def word_search(columns, table, search_column, search_term):
+def word_search(columns, table, search_column, search_term, type):
     # Creates a SELECT/FROM/WHERE/LIKE query against the database.
     mydb = mysql.connector.connect(
         host=connection.myhost,
@@ -17,17 +15,23 @@ def word_search(columns, table, search_column, search_term):
         database=connection.mydatabase,
         password=connection.mypassword
     )
+
     mycursor = mydb.cursor()
     mycursor.execute(
         "SELECT " + columns + " FROM " + table + " WHERE " + search_column + " LIKE '%" + search_term + "%'"
         # TODO improve search function https://dev.mysql.com/doc/refman/8.0/en/fulltext-search.html
     )
     myresult = mycursor.fetchall()
-    # TODO Output to csv
+    rownum=0
     for x in myresult:
-        print(x)
+        rownum=rownum+1
+        with open('/Users/jonathanleek/Desktop/working/'+type+'_'+search_term+'_'+str(rownum)+'.html', 'w') as out:
+            print(x, file=out)
     mycursor.close()
 
 for word in search_words:
-    print("Results for " + word)
-    word_search("author_name, post_title, post", "forums_posts", "post", word)
+    print("Public Post Results for " + word)
+    word_search("post", "forums_posts", "post", word,"public")
+    print("Private Message Results for " + word)
+    word_search("msg_post", "core_message_posts", "msg_post", word, "private")
+
